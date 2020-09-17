e9f01fffc18a23b5364ef73965f664cc9551986a
Iyris Vigil <iyrisvigil@gmail.com>
2020-09-17 22:04:30 +0000

    update getAllReservations function to use the lightbnb database with SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -8,6 +8,7 @@ const pool = new Pool({
 
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { query } = require('express');
 
 /// Users
 
@@ -76,8 +77,8 @@ const addUser =  function(user) {
   RETURNING *;
   `, [user.name, user.email, user.password])
   .then (
-    // res => res.rows[0]
-    console.log('myrows:', res.rows)
+    res => res.rows[0]
+    // console.log('myrows:', res.rows)
     )
 }
 exports.addUser = addUser;
@@ -90,7 +91,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date DESC
+  LIMIT $2;`, [guest_id, limit])
+  .then (res => 
+    res.rows
+  )
 }
 exports.getAllReservations = getAllReservations;
 
